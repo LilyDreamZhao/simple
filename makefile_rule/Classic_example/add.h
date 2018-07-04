f13918730a4129f95be8de30de5989b2a3c4f867
LilyDreamZhao <2932695153@qq.com>
2018-07-04 18:15:28 +0800

    add all file

--- makefile_rule/Classic_example/add.h
+++ makefile_rule/Classic_example/add.h
@@ -1,1 +1,5 @@
-ifndef
+#ifndef ADD_H
+#define ADD_H
+#include<stdio.h>
+int add(int a,int b);
+#endif
---
+++ makefile_rule/Classic_example/main.c
@@ -0,0 +1,8 @@
+#include"add.h"
+int add(int a,int b);
+int main(void){
+	int a=3;
+	int b=5;
+	printf("%d+%d=%d\n",a,b,add(a,b));
+	return 0;
+}
